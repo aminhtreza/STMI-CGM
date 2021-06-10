c7ad36eaf05fc9a8c5c7827973f03aba36d231b5
lucchik <lucchik@gmail.com>
2021-06-10 08:43:24 -0400

    working with great audios

--- STMI-CGM/ContentView.swift
+++ STMI-CGM/ContentView.swift
@@ -53,41 +53,52 @@ extension ContentView {
         let breakfast = UNMutableNotificationContent()
             breakfast.title = "WhatU8"
             breakfast.subtitle = "Breakfast"
-            breakfast.body = "What did you eat for breakfast?"
-            breakfast.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "breakfast.mp3"))
+            breakfast.body = "Kindly log what you had for breakfast in the app."
+            breakfast.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "breakfast_.mp3"))
+        
         var breakfastComponents = DateComponents()
-            breakfastComponents.hour = 9
-            breakfastComponents.minute = 0
+            //breakfastComponents.hour = 9
+            //breakfastComponents.minute = 0
+            breakfastComponents.hour = 8
+            breakfastComponents.minute = 32
+            breakfastComponents.second = 0
         let breakfastTrigger = UNCalendarNotificationTrigger(dateMatching: breakfastComponents, repeats: true)
         let breakfastRequest = UNNotificationRequest(identifier: UUID().uuidString, content: breakfast, trigger: breakfastTrigger)
-        center.add(breakfastRequest)
+        center.add(breakfastRequest, withCompletionHandler: nil)
         
         // lunch notification
         let lunch = UNMutableNotificationContent()
             lunch.title = "WhatU8"
             lunch.subtitle = "Lunch"
-            lunch.body = "What did you eat for lunch?"
-            lunch.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "lunch.mp3"))
+            lunch.body = "Kindly log what you had for lunch in the app."
+            lunch.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "lunch_.mp3"))
         var lunchComponents = DateComponents()
-            lunchComponents.hour = 14
-            lunchComponents.minute = 0
+            //lunchComponents.hour = 14
+            //lunchComponents.minute = 0
+            lunchComponents.hour = 8
+            lunchComponents.minute = 32
+            lunchComponents.second = 20
+            
         
         let lunchTrigger = UNCalendarNotificationTrigger(dateMatching: lunchComponents, repeats: true)
         let lunchRequest = UNNotificationRequest(identifier: UUID().uuidString, content: lunch, trigger: lunchTrigger)
-        center.add(lunchRequest)
+        center.add(lunchRequest, withCompletionHandler: nil)
         
         // dinner notification
         let dinner = UNMutableNotificationContent()
             dinner.title = "WhatU8"
             dinner.subtitle = "Dinner"
-            dinner.body = "What did you eat for dinner?"
-            dinner.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "dinner.mp3"))
+            dinner.body = "Kindly log what you had for dinner in the app."
+            dinner.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "dinner_.mp3"))
         var dinnerComponents = DateComponents()
-        dinnerComponents.hour = 19
-        dinnerComponents.minute = 0
+        //dinnerComponents.hour = 19
+        //dinnerComponents.minute = 0
+        dinnerComponents.hour = 8
+        dinnerComponents.minute = 32
+        dinnerComponents.second = 40
         let dinnerTrigger = UNCalendarNotificationTrigger(dateMatching: dinnerComponents, repeats: true)
         let dinnerRequest = UNNotificationRequest(identifier: UUID().uuidString, content: dinner, trigger: dinnerTrigger)
-        center.add(dinnerRequest)
+        center.add(dinnerRequest, withCompletionHandler: nil)
         
         print("requests added")
     }
